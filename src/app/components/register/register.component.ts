14589150317de37b7b8686ba551824497bbd6d01
Denison Kolling <denisonkolling@gmail.com>
2024-08-15 16:38:49 -0300

    feat(register): create register  form page

---
+++ src/app/components/register/register.component.ts
@@ -0,0 +1,56 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { REGEX } from '../../shared/constants/regex.constants';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [CardModule, InputTextModule, ReactiveFormsModule, ButtonModule, CommonModule, RouterModule],
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+
+
+  registerForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      fullName: ['', [
+        Validators.required,
+        // Validators.pattern(REGEX.EMAIL)
+      ]],
+      email: ['', [
+        Validators.required,
+        Validators.pattern(REGEX.EMAIL)
+      ]],
+      password: ['', [
+        Validators.required,
+        Validators.pattern(REGEX.PASSWORD)
+      ]],
+      confirmPassword: ['', [
+        Validators.required,
+        Validators.pattern(REGEX.PASSWORD)
+      ]]
+    });
+  }
+
+
+  get fullName() {
+    return this.registerForm.controls['fullName'];
+  }
+
+  get email() {
+    return this.registerForm.controls['email'];
+  }
+
+  get password() { return this.registerForm.controls['password']; }
+
+  get confirmPassword() { return this.registerForm.controls['confirmPassword']; }
+
+}
