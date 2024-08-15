be9522f9f3a0f7e04c7fc0b042b4fe42ef68c08b
Denison Kolling <denisonkolling@gmail.com>
2024-08-15 18:01:25 -0300

    feat(register): create succes or error toast info

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ToastModule],
+  providers: [MessageService, HttpClient],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
