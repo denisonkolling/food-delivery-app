cf1c3549a7b150410796cea9ef73b0140f0ff01a
Denison Kolling <denisonkolling@gmail.com>
2024-08-19 14:13:46 -0300

    feat(home): add home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,21 +1,26 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { AuthService } from '../../services/auth.service';
+import { HeaderComponent } from '../layout/header/header.component';
+import { SidebarComponent } from '../layout/sidebar/sidebar.component';
+import { FooterComponent } from '../layout/footer/footer.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ButtonModule],
+  imports: [ButtonModule, HeaderComponent, SidebarComponent, FooterComponent, RouterOutlet],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
 
-  constructor(private router: Router) { }
+  constructor(
+    private authService: AuthService
+  ) { }
 
   logout() {
-    sessionStorage.clear();
-    this.router.navigate(['login']);
+    this.authService.logoutUser();
   }
 
 }
