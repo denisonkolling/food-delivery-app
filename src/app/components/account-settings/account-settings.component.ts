3c224d89d09c7a6223af0bfee14e6f255294edc9
Denison Kolling <denisonkolling@gmail.com>
2024-08-22 16:15:03 -0300

    feat(profile): add change password form

--- src/app/components/account-settings/account-settings.component.ts
+++ src/app/components/account-settings/account-settings.component.ts
@@ -5,23 +5,36 @@ import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { InputTextModule } from 'primeng/inputtext';
 import { REGEX } from '../../shared/constants/regex.constants';
+import { HeaderComponent } from '../layout/header/header.component';
+import { SidebarComponent } from '../layout/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-account-settings',
   standalone: true,
-  imports: [CardModule, CommonModule, ReactiveFormsModule, InputTextModule, ButtonModule],
+  imports: [CardModule, CommonModule, ReactiveFormsModule, InputTextModule, ButtonModule, HeaderComponent, SidebarComponent,],
   templateUrl: './account-settings.component.html',
   styleUrl: './account-settings.component.css'
 })
 export class AccountSettingsComponent {
 
-  userForm!: FormGroup;
+  userForm: FormGroup;
+
+  passwordForm: FormGroup;
+
+  formChangePassword: boolean = false
+
+  formEditProfile: boolean = true
 
   constructor(private fb: FormBuilder) {
     this.userForm = this.fb.group({
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.pattern(REGEX.EMAIL)]],
       address: ['', Validators.required],
+    });
+
+    this.passwordForm = this.fb.group({
+      password: ['', [Validators.required, Validators.pattern(REGEX.PASSWORD)]],
+      passwordConfirm: ['', [Validators.required, Validators.pattern(REGEX.PASSWORD)]],
     });
   }
 
@@ -41,6 +54,27 @@ export class AccountSettingsComponent {
 
   get address() {
     return this.userForm.controls['address'];
+  }
+
+  btnShowFormChangePassword() {
+    this.formChangePassword = true
+    this.formEditProfile = false
+  }
+  btnShowFormEditProfile() {
+    this.formEditProfile = true
+    this.formChangePassword = false
+  }
+
+  onSubmitPasswordChange() {
+    throw new Error('Method not implemented.');
+  }
+
+  get password() {
+    return this.passwordForm.controls['password'];
+  }
+
+  get confirmPassword() {
+    return this.passwordForm.controls['passwordConfirm'];
   }
 
 }
