11d40617a3cbb2d5898700265f55fab4b010b436
Denison Kolling <denisonkolling@gmail.com>
2024-08-28 16:45:44 -0300

    refactor: account setting component remove botons change logic password form

--- src/app/components/account-settings/account-settings.component.ts
+++ src/app/components/account-settings/account-settings.component.ts
@@ -11,48 +11,15 @@ import { LayoutComponent } from '../layout/layout.component';
 import { ProfileCardComponent } from '../profile-card/profile-card.component';
 import { PaymentCardComponent } from '../payment-card/payment-card.component';
 import { ProfileFormComponent } from '../profile-form/profile-form.component';
+import { PasswordFormComponent } from '../password-form/password-form.component';
 
 @Component({
   selector: 'app-account-settings',
   standalone: true,
-  imports: [CardModule, CommonModule, ReactiveFormsModule, InputTextModule, ButtonModule, HeaderComponent, SidebarComponent, LayoutComponent, ProfileCardComponent, PaymentCardComponent, ProfileFormComponent],
+  imports: [CardModule, CommonModule, ReactiveFormsModule, InputTextModule, ButtonModule, HeaderComponent, SidebarComponent, LayoutComponent, ProfileCardComponent, PaymentCardComponent, ProfileFormComponent, PasswordFormComponent],
   templateUrl: './account-settings.component.html',
   styleUrl: './account-settings.component.css'
 })
 export class AccountSettingsComponent {
-
-  passwordForm: FormGroup;
-
-  formChangePassword: boolean = false
-
-  formEditProfile: boolean = true
-
-  constructor(private fb: FormBuilder) {
-    this.passwordForm = this.fb.group({
-      password: ['', [Validators.required, Validators.pattern(REGEX.PASSWORD)]],
-      passwordConfirm: ['', [Validators.required, Validators.pattern(REGEX.PASSWORD)]],
-    });
-  }
-
-  btnShowFormChangePassword() {
-    this.formChangePassword = true
-    this.formEditProfile = false
-  }
-  btnShowFormEditProfile() {
-    this.formEditProfile = true
-    this.formChangePassword = false
-  }
-
-  onSubmitPasswordChange() {
-    throw new Error('Method not implemented.');
-  }
-
-  get password() {
-    return this.passwordForm.controls['password'];
-  }
-
-  get confirmPassword() {
-    return this.passwordForm.controls['passwordConfirm'];
-  }
 
 }
