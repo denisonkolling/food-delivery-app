60e289e7f9e6e4723142c8351e89c85e061d8f90
Denison Kolling <denisonkolling@gmail.com>
2024-08-28 18:12:12 -0300

    feat(payment): change expiry date calendar to prime ng component

--- src/app/components/payment-card/payment-card.component.ts
+++ src/app/components/payment-card/payment-card.component.ts
@@ -2,13 +2,14 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
+import { CalendarModule } from 'primeng/calendar';
 import { CardModule } from 'primeng/card';
 import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-payment-card',
   standalone: true,
-  imports: [CardModule, CommonModule, ButtonModule, FormsModule, ReactiveFormsModule, InputTextModule],
+  imports: [CardModule, CommonModule, ButtonModule, FormsModule, ReactiveFormsModule, InputTextModule, CalendarModule],
   templateUrl: './payment-card.component.html',
   styleUrls: ['./payment-card.component.scss']
 })
