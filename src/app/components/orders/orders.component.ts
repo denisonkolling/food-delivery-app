579530b1e2849609875386a68c0d21ec7c094b43
Denison Kolling <denisonkolling@gmail.com>
2024-08-29 16:30:02 -0300

    feat (orders): update create order component

--- src/app/components/orders/orders.component.ts
+++ src/app/components/orders/orders.component.ts
@@ -2,17 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../layout/header/header.component';
 import { SidebarComponent } from '../layout/sidebar/sidebar.component';
 import { FooterComponent } from '../layout/footer/footer.component';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink } from '@angular/router';
 import { OrdersTableComponent } from '../orders-table/orders-table.component';
-import { OrdersCreateComponent } from '../orders-create/orders-create.component';
 import { ButtonModule } from 'primeng/button';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from '../layout/layout.component';
+import { OrderService } from '../../services/order.service';
+import { OrderCreateComponent } from '../order-create/orders-create.component';
+
 
 @Component({
   selector: 'app-orders',
   standalone: true,
-  imports: [HeaderComponent, SidebarComponent, FooterComponent, RouterLink, OrdersTableComponent, OrdersCreateComponent, ButtonModule, CommonModule, LayoutComponent],
+  imports: [HeaderComponent, SidebarComponent, FooterComponent, RouterLink, OrdersTableComponent, ButtonModule, CommonModule, LayoutComponent, OrderCreateComponent],
+  providers: [OrderService],
   templateUrl: './orders.component.html',
   styleUrl: './orders.component.css'
 })
