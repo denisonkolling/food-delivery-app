d2c5fd560a87571e3319ecfdf02ef6540c5fba4a
Denison Kolling <denisonkolling@gmail.com>
2024-08-30 16:38:01 -0300

    style: update layout responsiveness

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from '../layout/header/header.component';
 import { SidebarComponent } from '../layout/sidebar/sidebar.component';
 import { FooterComponent } from '../layout/footer/footer.component';
 import { RouterOutlet } from '@angular/router';
+import { LayoutComponent } from '../layout/layout.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [ButtonModule, HeaderComponent, SidebarComponent, FooterComponent, RouterOutlet],
+  imports: [ButtonModule, HeaderComponent, SidebarComponent, FooterComponent, RouterOutlet, LayoutComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -5,11 +5,12 @@ import { HeaderComponent } from '../layout/header/header.component';
 import { SidebarComponent } from '../layout/sidebar/sidebar.component';
 import { FooterComponent } from '../layout/footer/footer.component';
 import { RouterOutlet } from '@angular/router';
+import { LayoutComponent } from '../layout/layout.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ButtonModule, HeaderComponent, SidebarComponent, FooterComponent, RouterOutlet],
+  imports: [ButtonModule, HeaderComponent, SidebarComponent, FooterComponent, RouterOutlet, LayoutComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
