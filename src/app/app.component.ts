5a8b677783e8bf5908dfb4de9dce76d0b4f796d2
Denison Kolling <denisonkolling@gmail.com>
2024-09-14 15:00:53 -0300

    feat(auth): implement http interceptor to provide  jwt token on requests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToastModule } from 'primeng/toast';
 import { MessageService } from 'primeng/api';
-import { HttpClient } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
+import { JwtInterceptor } from './interceptors/jwt.interceptor';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, ToastModule],
-  providers: [MessageService, HttpClient],
+  providers: [MessageService, HttpClient, { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
---
+++ src/app/interceptors/jwt.interceptor.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class JwtInterceptor implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token = sessionStorage.getItem('access_token');
+    if (token) {
+      const cloned = req.clone({
+        headers: req.headers.set('Authorization', `Bearer ${token}`)
+      });
+      return next.handle(cloned);
+    }
+    return next.handle(req);
+  }
+}
