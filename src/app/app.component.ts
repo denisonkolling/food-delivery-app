8b8012ca4ee9009321bb40a713141fc7912568bd
Denison Kolling <denisonkolling@gmail.com>
2024-09-15 19:18:45 -0300

    fix: declare jwt interceptor config in right way

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToastModule } from 'primeng/toast';
 import { MessageService } from 'primeng/api';
-import { HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
-import { JwtInterceptor } from './interceptors/jwt.interceptor';
-
-
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, ToastModule],
-  providers: [MessageService, HttpClient, { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }],
+  providers: [MessageService],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,9 +3,22 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { JwtInterceptor } from './interceptors/jwt.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch()), provideAnimations()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(withFetch()),
+    provideAnimations(),
+    provideHttpClient(withInterceptorsFromDi()),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: JwtInterceptor,
+      multi: true
+    }
+  ]
 };
