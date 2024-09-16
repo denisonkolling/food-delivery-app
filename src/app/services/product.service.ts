e7ab187cbb044150c25ab5516dca6fca99f34b76
Denison Kolling <denisonkolling@gmail.com>
2024-09-16 15:18:43 -0300

    rename create product

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -16,7 +16,7 @@ export class ProductService {
         return this.http.get<Product[]>(`${BASE_URL.BASE_URL}/products`);
     }
 
-    addProduct(data: any): Observable<any> {
+    createProduct(data: any): Observable<any> {
         return this.http.post(`${BASE_URL.BASE_URL}/products`, data);
     }
 
